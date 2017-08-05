1c46343b16b7dd5946d0d68f87e737a6939cdb8a
rakai <ra9g16@soton.ac.uk>
2017-08-05 17:14:06 +0200

    Initial Commit

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Ryota Akai on 2017/08/02.
+//  Copyright © 2017年 ryota.akai. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
